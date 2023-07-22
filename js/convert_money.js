f36a6aab64b7cef3280e7ec71d1aaba4b8bb0d82
Tapishagrawal <tapishg4@gmail.com>
2023-07-22 21:27:27 +0530

    convertor styling part done

--- js/convert_money.js
+++ js/convert_money.js
@@ -1,6 +1,7 @@
 
 
-function con() {fetch("https://restcountries.com/v2/all")
+function con() {
+  fetch("https://restcountries.com/v2/all")
   .then(response => response.json())
   .then(data => {
     const countries = data;
@@ -9,11 +10,11 @@ function con() {fetch("https://restcountries.com/v2/all")
     const fromCurrencySelect = document.getElementById("fromMoney");
     const toCurrencySelect = document.getElementById("toMoney");
 
-    countries.forEach(({currencies,flags,name})=> {
+    countries.forEach(({ currencies, flags, name }) => {
       const option = document.createElement("option");
-      for(let k in currencies){
+      for (let k in currencies) {
         option.value = currencies[k].code;
-        option.nodeValue=name
+        option.nodeValue = name
       }
       option.textContent = `${name} (${option.value})`;
 
@@ -25,61 +26,57 @@ function con() {fetch("https://restcountries.com/v2/all")
     console.log("Failed to fetch country data.", error);
   });
 
-
-
-  
 }
 con();
 
 
-document.getElementById("fromMoney").addEventListener('change',()=>{
-  document.getElementById("amount").value=0;
-  document.getElementById("result").value=0;
+document.getElementById("fromMoney").addEventListener('change', () => {
+  document.getElementById("amount").value = 0;
+  document.getElementById("result").value = 0;
 
 
 })
 
 
 function convertMoney() {
-    var amount = document.getElementById("amount").value;
-  
-    let fromMoney = document.getElementById("fromMoney").value;
-   let toMoney = document.getElementById("toMoney").value;
-  
-    const endpoint = `https://api.exchangerate-api.com/v4/latest/USD`; // USD is base Money
-  
-      fetch(endpoint)
-      .then(response => response.json())
-      .then(data => {
-        const rates = data.rates;
-       
-        if (fromMoney !==toMoney) {
-            let x=amount / rates[fromMoney];
-          amount = Number.parseFloat(x).toFixed(2)
-        }
-       // console.log(data.rates[toMoney]);
-        //amount = Math.round(amount * rates[toMoney] * 100) / 100;
-        let x=(amount * rates[toMoney] * 100) / 100;
+  var amount = document.getElementById("amount").value;
+
+  let fromMoney = document.getElementById("fromMoney").value;
+  let toMoney = document.getElementById("toMoney").value;
+
+  const endpoint = `https://api.exchangerate-api.com/v4/latest/USD`; // USD is base Money
+
+  fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+      const rates = data.rates;
+
+      if (fromMoney !== toMoney) {
+        let x = amount / rates[fromMoney];
         amount = Number.parseFloat(x).toFixed(2)
-        console.log(amount);
-        document.getElementById("result").value = `${amount}`;
+      }
+      // console.log(data.rates[toMoney]);
+      //amount = Math.round(amount * rates[toMoney] * 100) / 100;
+      let x = (amount * rates[toMoney] * 100) / 100;
+      amount = Number.parseFloat(x).toFixed(2)
+      console.log(amount);
+      document.getElementById("result").value = `${amount}`;
 
-        let temp1,temp2;
+      let temp1, temp2;
 
-        let  z= 1/rates[fromMoney];
-        temp2= Number.parseFloat(z).toFixed(5);
-       
-        let  y=rates[fromMoney] ;
-        temp1= Number.parseFloat(y).toFixed(5);
+      let z = 1 / rates[fromMoney];
+      temp2 = Number.parseFloat(z).toFixed(5);
 
-       
+      let y = rates[fromMoney];
+      temp1 = Number.parseFloat(y).toFixed(5);
 
-        document.getElementById("result2").textContent = `1 ${fromMoney} = ${temp2} ${toMoney}  And
+
+
+      document.getElementById("result2").textContent = `1 ${fromMoney} = ${temp2} ${toMoney}  And
         1 ${toMoney} = ${temp1} ${fromMoney}`;
 
-      })
-      .catch(error => {
-        console.log("Failed to fetch exchange rates.", error);
-      });
-  }
-  
+    })
+    .catch(error => {
+      console.log("Failed to fetch exchange rates.", error);
+    });
+}
